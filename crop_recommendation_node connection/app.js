7b5ea283e544d17a7a657068b50ad4665c761da8
Devil <singhsansar433@gmail.com>
2023-03-24 11:59:53 +0530

    completed

--- crop_recommendation_node connection/app.js
+++ crop_recommendation_node connection/app.js
@@ -1,5 +1,6 @@
 //connection Node with the ML model in python 
 // const asyncio = require("asy")
+const http = require('http')
 const ejs = require('ejs')
 const {get_crop} = require('./result')
 const express = require('express')
@@ -9,8 +10,10 @@ const {spawn} = require('child_process');
 const { assert } = require('console');
 const port = process.env.PORT | 4100 
 
+const server = http.createServer(app)
 
-app.use(express.json())/
+
+app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 
@@ -18,21 +21,20 @@ app.set('view engine','ejs')//set a view-engine that can be use to generate the 
 app.set('views',path.join(__dirname,'views'))
 
 app.post('/getcrop',(req,res)=>{
-    const N = Number(req.body.N)
-    const P = Number(req.body.P)
-    const K = Number(req.body.K)
-    const Ph = Number(req.body.Ph)
-    const T = Number(req.body.T)
-    var arr = [N,P,K,Ph,T]
-    get_crop(arr).then((data)=>
+    // const N = Number(req.body.N)
+    // const P = Number(req.body.P)
+    // const K = Number(req.body.K)
+    // const Ph = Number(req.body.Ph)
+    // const T = Number(req.body.T)
+    // var arr = [N,P,K,Ph,T]
+    const data = req.body.arr 
+    var numberArray = data.reduce( (acc, x ) => acc.concat(+x), [])
+    get_crop(numberArray).then((data)=>
     {
         var crop = data 
         console.log(crop)
 
-        res.render('result'
-        ,{
-            crop_recom: crop,}
-        )
+        res.send(crop)
       })
    
 })
@@ -45,7 +47,7 @@ app.get('/',(req,res)=>{
     res.render('index')})
 
 
-app.listen(port,()=>
+server.listen(port,()=>
 {
     console.log(`http://localhost:${port}`)
 })
