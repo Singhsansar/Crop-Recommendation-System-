23f59ff50b3e4155cde29d439c88a7009a03450c
Nikhil <singhsansar433@gmail.com>
2023-11-02 21:40:23 +0530

    makes changed in codes

--- crop_recommendation_node connection/app.js
+++ crop_recommendation_node connection/app.js
@@ -1,55 +1,33 @@
-//connection Node with the ML model in python 
-// const asyncio = require("asy")
-const http = require('http')
-const ejs = require('ejs')
-const {get_crop} = require('./result')
-const express = require('express')
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const { get_crop } = require('./result');
+
 const app = express();
-const path = require('path')
-const {spawn} = require('child_process');
-const { assert } = require('console');
-const port = process.env.PORT | 4100 
+const port = process.env.PORT || 4200;
 
-const server = http.createServer(app)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
+app.post('/getcrop', async (req, res) => {
+    try {
+        const data = req.body.arr.map(Number);
+        const crop = await get_crop(data);
+        console.log(crop);
+        res.status(200).send(crop);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
-
-app.set('view engine','ejs')//set a view-engine that can be use to generate the web-templates 
-app.set('views',path.join(__dirname,'views'))
-
-app.post('/getcrop',(req,res)=>{
-    // const N = Number(req.body.N)
-    // const P = Number(req.body.P)
-    // const K = Number(req.body.K)
-    // const Ph = Number(req.body.Ph)
-    // const T = Number(req.body.T)
-    // var arr = [N,P,K,Ph,T]
-    const data = req.body.arr 
-    var numberArray = data.reduce( (acc, x ) => acc.concat(+x), [])
-    get_crop(numberArray).then((data)=>
-    {
-        var crop = data 
-        console.log(crop)
-
-        res.send(crop)
-      })
-   
-})
-
-
-
-
-
-app.get('/',(req,res)=>{
-    res.render('index')})
-
-
-server.listen(port,()=>
-{
-    console.log(`http://localhost:${port}`)
-})
-
-
+const server = http.createServer(app);
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
