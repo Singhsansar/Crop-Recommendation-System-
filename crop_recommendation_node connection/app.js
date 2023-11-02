c89344c25892b92216aca1a22f9ca97ac123bde3
Nikhil <singhsansar433@gmail.com>
2023-11-02 23:56:40 +0530

    change in requirements

--- crop_recommendation_node connection/app.js
+++ crop_recommendation_node connection/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { get_crop } = require('./result');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
